57fb1b88667b4a4466124365358a09fe2a3e7f7b
haim-petcherski <haimpiche@gmail.com>
2021-01-15 17:58:06 +0200

    .

--- Ex4/client/main_client.c
+++ Ex4/client/main_client.c
@@ -3,14 +3,166 @@
 
 #include "main_client.h"
 
+Status connect_level(SOCKADDR_IN client_service, int server_port, long server_address, SEND_SERVER* send_server, CLIENT_ACTION* client_action)
+{
+	char send_str[1];
+	while (1)
+	{
+		if (connect(m_socket, (SOCKADDR*)&client_service, sizeof(client_service)) == SOCKET_ERROR)
+		{
+			printf("Failed connecting to server on %ld:%d.\nChoose what to do next:\n1. Try to reconnect\2. Exit", server_address, server_port);
+			gets_s(send_str, sizeof(send_str)); //Reading a string from the keyboard
+			if (STRINGS_ARE_EQUAL(send_str, "1"))
+				continue;
+			else if (STRINGS_ARE_EQUAL(send_str, "2"))
+			{
+				return USER_EXIT;
+			}
+		}
+		else
+			break;
+	}
+	*send_server = CLIENT_REQUEST;
+	*client_action = SEND;
+	return SUCCESS;
+}
+
+Status SendBuffer(const char* Buffer, int BytesToSend, SOCKET sd)
+{
+	const char* CurPlacePtr = Buffer;
+	int BytesTransferred;
+	int RemainingBytesToSend = BytesToSend;
+
+	while (RemainingBytesToSend > 0)
+	{
+		/* send does not guarantee that the entire message is sent */
+		BytesTransferred = send(sd, CurPlacePtr, RemainingBytesToSend, 0);
+		if (BytesTransferred == SOCKET_ERROR)
+		{
+			return FAILED_SEND;
+		}
+
+		RemainingBytesToSend -= BytesTransferred;
+		CurPlacePtr += BytesTransferred; // <ISP> pointer arithmetic
+	}
+
+	return SUCCESS;
+}
+
+Status send_string(const char* Str, SOCKET sd)
+{
+	/* Send the the request to the server on socket sd */
+	int TotalStringSizeInBytes;
+	Status SendRes;
+
+	/* The request is sent in two parts. First the Length of the string (stored in
+	   an int variable ), then the string itself. */
+
+	TotalStringSizeInBytes = (int)(strlen(Str) + 1); // terminating zero also sent	
+
+	SendRes = SendBuffer(
+		(const char*)(&TotalStringSizeInBytes),
+		(int)(sizeof(TotalStringSizeInBytes)), // sizeof(int) 
+		sd);
+
+	if (SendRes != SUCCESS) return SendRes;
+
+	SendRes = SendBuffer(
+		(const char*)(Str),
+		(int)(TotalStringSizeInBytes),
+		sd);
+
+	return SendRes;
+}
+
+Status send_level(char* player_name, SOCKET m_socket, SEND_SERVER* send_server, RECEIVE_SERVER receive_server, CLIENT_ACTION* client_action, int server_port, long server_address)
+{
+	char send_str[MAX_LEN………_SEND];
+	if (*send_server == CLIENT_REQUEST)
+	{
+		*client_action = RECEIVE;
+		return send_string(player_name, m_socket);
+	}
+	if (receive_server == SERVER_MAIN_MENU)
+	{
+		printf("Choose what to do next:\n1. Play against another client\n2. Quit");
+		gets_s(send_str, sizeof(send_str));
+		*client_action = RECEIVE;
+		if (!strcmp(send_str, "2"))
+		{
+			send_string("CLIENT……_DISCONNECT\n", m_socket);
+			return USER_QUIT;
+		}
+		*send_server == CLIENT_VERSUS;
+		return send_string("CLIENT……_VERSUS\n", m_socket);
+	}
+	if (receive_server == SERVER_INVITE)// moove to receive block
+	{
+		printf("Game is on!");
+		*client_action = RECEIVE;
+		return SUCCESS;
+	}
+	if (receive_server == SERVER_DENIED)
+	{
+		closesocket(m_socket);
+		printf("Server on %ld : %d denied the connection request.\nChoose what to do next :\n1. Try to reconnect\n2. Exit", server_address, server_port);
+		gets_s(send_str, sizeof(send_str));
+		if (!strcmp(send_str,"2"))
+		{
+			return USER_EXIT;
+		}
+		else
+		{
+			client_action = CONNECT;
+			return SUCCESS;
+		}
+	}
+	if (receive_server == SERVER_SETUP_REQUEST)
+	{
+		printf("Choose your 4 digits:");
+		gets_s(send_str, sizeof(send_str));
+		*client_action = RECEIVE;
+		return send_string(send_str, m_socket);
+	}
+	if (receive_server == SERVER_SETUP_REQUEST)
+	{
+		printf("Choose your 4 digits:");
+		gets_s(send_str, sizeof(send_str));
+		*client_action = RECEIVE;
+		return send_string(sprintf_s(send_str, MAX_LEN………_SEND, "CLIENT_SETUP:%s\n", send_str), m_socket);
+	}
+	if (receive_server == SERVER_PLAYER_MOVE_REQUEST)
+	{
+		printf("Choose your guess:");
+		gets_s(send_str, sizeof(send_str));
+		*client_action = RECEIVE;
+		return send_string(sprintf_s(send_str, MAX_LEN………_SEND, "CLIENT_PLAYER_MOVE:%s\n", send_str), m_socket);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
 Status main(int argc, char* argv[])
 {
-	HANDLE hThread;
-	int server_port;
-	THREAD_ARGS thread_args;
+	char player_name[20];
+	CLIENT_ACTION client_action = SEND;
+	Status status = SUCCESS;
+	SOCKADDR_IN client_service;
+	SEND_SERVER send_server = CLIENT_REQUEST;
 	int iResult;
 	DWORD event_result;
-	unsigned long server_address;
+	int server_port;
+	long server_address;
+	RECEIVE_SERVER receive_server;
+	char send_str[1];
 	WSADATA wsaData; //Create a WSADATA object called wsaData.
 //The WSADATA structure contains information about the Windows Sockets implementation.
 
@@ -20,13 +172,13 @@ Status main(int argc, char* argv[])
 	}
 	server_port = atoi(argv[SERVER_PORT]);
 	// if we want to give the player another chance need to add here while(1) loop
-	if (strlen(argv[2]) != SERVER_PORT_LENTH)
+	if (strlen(argv[2]) != SERVER_PORT_LENGTH)
 	{
 		printf("Port server number is illegal try again");
 		return ILLEGAL_PORT;
 	}
 
-	strcpy_s(thread_args.player_name, MAX_PLAYER_NAME, argv[PLAYER_NAME]);
+	strcpy_s(player_name, MAX_PLAYER_NAME, argv[PLAYER_NAME]);
 	server_address = inet_addr(argv[SERVER_ADDRESS]);
 	if (server_address == INADDR_NONE)
 	{
@@ -43,7 +195,7 @@ Status main(int argc, char* argv[])
 		printf("Error at WSAStartup()\n");
 		return WSASTARTUP_FAILED;
 	}
-	    
+
 
 	//Call the socket function and return its value to the m_socket variable. 
 	// For this application, use the Internet address family, streaming sockets, and the TCP/IP protocol.
@@ -57,79 +209,76 @@ Status main(int argc, char* argv[])
 		WSACleanup();
 		return FAILED_CREATE_SOCKET;
 	}
-	/*
-	 The parameters passed to the socket function can be changed for different implementations.
-	 Error detection is a key part of successful networking code.
-	 If the socket call fails, it returns INVALID_SOCKET.
-	 The if statement in the previous code is used to catch any errors that may have occurred while creating
-	 the socket. WSAGetLastError returns an error number associated with the last error that occurred.
-	 */
 
+	client_service.sin_family = AF_INET;
+	client_service.sin_addr.s_addr = server_port; //Setting the IP address to connect to
+	client_service.sin_port = htons(server_port); //Setting the port to connect to.
 
-	 //For a client to communicate on a network, it must connect to a server.
-	 // Connect to a server.
-
-	 //Create a sockaddr_in object clientService and set  values.
-	thread_args.clientService.sin_family = AF_INET;
-	thread_args.clientService.sin_addr.s_addr =server_address; //Setting the IP address to connect to
-	thread_args.clientService.sin_port = htons(server_port); //Setting the port to connect to.
-
-
-	/*
-		AF_INET is the Internet address family.
-	*/
-	close_event = CreateEvent(
-		NULL,               // default security attributes
-		TRUE,               // manual-reset event
-		FALSE,              // initial state is nonsignaled
-		TEXT("close_event")  // object name
-	);
-
-	open_event = CreateEvent(
-		NULL,               // default security attributes
-		TRUE,               // manual-reset event
-		FALSE,              // initial state is nonsignaled
-		TEXT("open_event")  // object name
-	);
-
-	if (open_event == NULL || close_event == NULL)
+	while (1)
 	{
-		printf("fail to create an Event %d", GetLastError());
-		return FAILED_EVENT;
-	}
-
-
-	hThread = CreateThread(
-			NULL,
-			0,
-			(LPTHREAD_START_ROUTINE)communication_thread,
-			&thread_args,
-			0,
-			NULL
-		);
-	while (1)//main thread as a timer for the communication thread
-	{
-
-		event_result = WaitForSingleObject(
-			open_event, // event handle
-			time_out);    // time wait is dependent on the scenario in communication threa
-
-
-
-
-			// An error occurred
-		if (event_result != WAIT_OBJECT_0)
+		if (connect(m_socket, (SOCKADDR*)&client_service, sizeof(client_service)) == SOCKET_ERROR)
 		{
-			printf("Wait error (%d)\n", GetLastError());
-			return 0;
+			printf("Failed connecting to server on %ld:%d.\nChoose what to do next:\n1. Try to reconnect\2. Exit", server_address, server_port);
+			gets_s(send_str, sizeof(send_str)); //Reading a string from the keyboard
+			if (STRINGS_ARE_EQUAL(send_str, "1"))
+				continue;
+			else if (STRINGS_ARE_EQUAL(send_str, "2"))
+			{
+				report_error(FAILED_CONNECT);
+			}
 		}
-
-
-		event_result = WaitForSingleObject(
-			close_event, // event handle
-			time_out);    // time wait is dependent on the scenario in communication threa
-
-
-		//if()
+		else
+			break;
 	}
+
+	while (status!=USER_QUIT && status!=USER_EXIT)
+	{
+		//connect block
+		if (client_action == CONNECT)
+		{
+			status = connect_level(client_service, server_port, server_address, &send_server, &client_action);
+			if (status != SUCCESS && status != USER_QUIT && status != USER_EXIT)
+				report_error(status);
+			continue;
+		}
+		//send block
+		if (client_action == SEND)
+		{
+			setsockopt(m_socket, SOL_SOCKET, SO_SNDTIMEO, TIMEOUT_SEND, sizeof(int));
+			status = send_level(player_name, m_socket, &send_server, receive_server, &client_action, server_port, server_address);
+			if (status != SUCCESS)
+				report_error(status);
+			continue;
+		}
+		//recv block
+		if (client_action == RECEIVE)
+		{
+			if (send_server == CLIENT_VERSUS)
+				setsockopt(m_socket, SOL_SOCKET, SO_SNDTIMEO, TIMEOUT_RECEIVE_LONG, sizeof(int));
+			else
+				setsockopt(m_socket, SOL_SOCKET, SO_SNDTIMEO, TIMEOUT_RECEIVE_SHORT, sizeof(int));
+			char* AcceptedStr = NULL;
+			status = receive_level(player_name, m_socket, send_server, receive_server, &client_action);
+			if (status != SUCCESS)
+				report_error(status);
+			continue;
+		}
+	}
+	status = SUCCESS;
+
+	closesocket(m_socket);
+	WSACleanup();
+
+
+
+	return SUCCESS;
+}
+
+void report_error(Status status) {
+	closesocket(m_socket);
+	WSACleanup();
+	switch (status) {
+	case FAILED_SEND:         printf("Error - Failed at send_function %ld", WSAGetLastError()); break;
+	}
+	exit(status);
 }
--- Ex4/client/main_client.h
+++ Ex4/client/main_client.h
@@ -7,6 +7,13 @@
 #ifndef __MAIN_CLIENT_H__
 #define __MAIN_CLIENT_H__
 
+#define BRUTAL_TERMINATION_CODE 0x55
+#define STRINGS_ARE_EQUAL( Str1, Str2 ) ( strcmp( (Str1), (Str2) ) == 0 )
+#define TIMEOUT_SEND 30000 
+#define TIMEOUT_RECEIVE_SHORT 15000
+#define TIMEOUT_RECEIVE_LONG 30000
+
+
 //Includes:
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,24 +22,47 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include "HardCodedData.h"
-#include "thread.h"
 #include <stdbool.h> 
 #pragma comment(lib, "Ws2_32.lib")
 
 //Typedefs:
+typedef enum _client_action {
+	CONNECT,
+	SEND,
+	RECEIVE,
+	DISCONNECT
+} CLIENT_ACTION;
 
-typedef struct _thread_args {
-    char player_name[MAX_PLAYER_NAME];
-    SOCKADDR_IN clientService;
-}THREAD_ARGS;
+typedef enum _send_server {
+	CLIENT_REQUEST,
+	CLIENT_VERSUS,
+	CLIENT_SETUP,
+	CLIENT_PLAYER_MOVE,
+	CLIENT_DISCONNECT
+} SEND_SERVER;
+
+typedef enum _receive_server {
+	SERVER_MAIN_MENU,
+	SERVER_APPROVED,
+	SERVER_DENIED,
+	SERVER_INVITE,
+	SERVER_SETUP_REQUEST,
+	SERVER_PLAYER_MOVE_REQUEST,
+	SERVER_GAME_RESULTS,
+	SERVER_WIN,
+	SERVER_DRAW,
+	SERVER_NO_OPPONENTS,
+	SERVER_OPPONENT_QUIT
+} RECEIVE_SERVER;
 
 
 
 //Global Variables:
-//Syncronization barrier to sync threading
 HANDLE open_event;
 HANDLE close_event;
+HANDLE user_event;
 SOCKET m_socket;
+bool new_communication_thread = true;
 int time_out;
 
 
@@ -43,5 +73,10 @@ int time_out;
 //Function Handles:
 //Prints errors according to the recieved status
 Status main(int argc, char* argv[]);
+void report_error(Status status);
+Status send_level(char* player_name, SOCKET m_socket, SEND_SERVER* send_server, RECEIVE_SERVER receive_server, CLIENT_ACTION* client_action, int server_port, long server_address);
+Status connect_level(SOCKADDR_IN client_service, int server_port, long server_address, SEND_SERVER* send_server, CLIENT_ACTION* client_action);
+Status send_string(const char* Str, SOCKET sd);
+Status SendBuffer(const char* Buffer, int BytesToSend, SOCKET sd);
 
 #endif // __MAIN_H__
